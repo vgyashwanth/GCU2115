dd5652688f9e9fcaf54f68191a9b8b120aff3bd1
SuryaPranayTeja BVV <suryapranayteja.bvv@sedemac.net>
2022-10-04 17:57:54 +0530

    Build for R0.3 version build

--- apl/APL_Manager/APL_Manager.h
+++ apl/APL_Manager/APL_Manager.h
@@ -55,7 +55,7 @@ private:
        uint32_t u32PLMID;
        uint16_t u16Dummy;
     }INFO_t;
-     INFO_t _stFirmwareInfo={0,2,0};
+     INFO_t _stFirmwareInfo={0,3,0};
     void prvCheckFirmwareInfo();
 };
 

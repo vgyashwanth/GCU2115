56f2d9920a26455c1ac9d339f250ede25ed22fb5
SuryaPranayTeja BVV <suryapranayteja.bvv@sedemac.net>
2022-11-01 10:58:04 +0530

    The increment range for sensors corrected and Drop for R0V07 drop

--- apl/APL_Manager/APL_Manager.h
+++ apl/APL_Manager/APL_Manager.h
@@ -55,7 +55,7 @@ private:
        uint32_t u32PLMID;
        uint16_t u16Dummy;
     }INFO_t;
-     INFO_t _stFirmwareInfo={0,6,0};
+     INFO_t _stFirmwareInfo={0,7,0};
     void prvCheckFirmwareInfo();
 };
 

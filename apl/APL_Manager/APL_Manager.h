d56c6885d18aee24b157dcdc5aaa52da8ab47039
Shibu Kumar <shibu.kumar@sedemac.com>
2023-11-27 15:20:37 +0530

    Firmware Version Updated

--- apl/APL_Manager/APL_Manager.h
+++ apl/APL_Manager/APL_Manager.h
@@ -55,7 +55,7 @@ private:
        uint32_t u32PLMID;
        uint16_t u16Dummy;
     }INFO_t;
-     INFO_t _stFirmwareInfo={2,1,13651};
+     INFO_t _stFirmwareInfo={2,2,13651};
     void prvCheckFirmwareInfo();
 };
 

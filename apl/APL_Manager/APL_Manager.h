4d801baa729bdbc1fbfb7ce52b5bb03c77daa3d0
aditya.joshi <aditya.joshi@sedemac.com>
2024-01-09 13:07:04 +0530

    Updated firmware revision and version to R0V00

--- apl/APL_Manager/APL_Manager.h
+++ apl/APL_Manager/APL_Manager.h
@@ -55,7 +55,7 @@ private:
        uint32_t u32PLMID;
        uint16_t u16Dummy;
     }INFO_t;
-     INFO_t _stFirmwareInfo={2,3,13651};
+     INFO_t _stFirmwareInfo={0, 0, 13651};
     void prvCheckFirmwareInfo();
 };
 
